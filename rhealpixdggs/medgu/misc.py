6c2c8a47ed603c5dbf82593f4a3e33ffd720871c
Frane Gilic <fgilic@gradst.hr>
2024-10-25 14:48:23 +0200

    Reformat code and add Jupyter lab notebook

--- rhealpixdggs/medgu/misc.py
+++ rhealpixdggs/medgu/misc.py
@@ -8,7 +8,6 @@ import rioxarray
 from itertools import chain
 
 
-
 # cells = rdggs.cells_from_region(resolution=5, ul=(16.45011,43.51516), dr=(16.46,43.51263), plane=False)
 # cells_data = []
 # for row in cells:
@@ -20,31 +19,30 @@ from itertools import chain
 # gdf = gpd.GeoDataFrame(cells_data, crs="EPSG:4326")
 # gdf.to_parquet("test.parquet")
 
-
-
-
-
-
-
 if __name__ == "__main__":
     client = Client(processes=True, n_workers=8)
+
     def func(strips):
         ul, lr = strips
-        cells = rdggs.cells_from_region(
-            5, ul=ul, dr=lr, plane=False
-        )
+        cells = rdggs.cells_from_region(5, ul=ul, dr=lr, plane=False)
         centroids = []
 
-        # tick = 0
-        # percentage = 0
-        # total = len(cells)
         for row in cells:
-            print(9)
             for cell in row:
                 centroid = {}
                 centroid["geometry"] = Point(cell.nucleus(plane=False))
-                value_2010 = rds.sel(x=centroid["geometry"].x, y=centroid["geometry"].y, band=11, method="nearest").item()
-                value_2020 = rds.sel(x=centroid["geometry"].x, y=centroid["geometry"].y, band=21, method="nearest").item()
+                value_2010 = rds.sel(
+                    x=centroid["geometry"].x,
+                    y=centroid["geometry"].y,
+                    band=11,
+                    method="nearest",
+                ).item()
+                value_2020 = rds.sel(
+                    x=centroid["geometry"].x,
+                    y=centroid["geometry"].y,
+                    band=21,
+                    method="nearest",
+                ).item()
 
                 if value_2010 == 190 or value_2020 == 190:
                     centroid["suid"] = str(cell)
@@ -54,27 +52,14 @@ if __name__ == "__main__":
                         centroid["built_up_2010"] = 1
                     if value_2020 == 190:
                         centroid["built_up_2020"] = 1
-
                     centroids.append(centroid)
-            # tick += 1
-            # print("a")
-            # if int((tick/total)*100) > percentage:
-            #     percentage += 1
-            #     print(f"{percentage}%")
-        print(10)
         return centroids
-
 
     ellipsoid = WGS84_ELLIPSOID
     rdggs = RHEALPixDGGS(ellipsoid=ellipsoid, N_side=13, north_square=0, south_square=0)
 
-    # file = "testno_2.tif"
     file = "GLC_FCS30D_clip.tif"
     rds = rioxarray.open_rasterio(file)
-    # raster_file = rasterio.open(file)
-    # raster_2010 = raster_file.read(11)
-    # raster_2020 = raster_file.read(21)
-    # raster = raster_file.read(11)
 
     ul = (rds.coords["x"].min().item(), rds.coords["y"].max().item())
     lr = (rds.coords["x"].max().item(), rds.coords["y"].min().item())
@@ -90,7 +75,7 @@ if __name__ == "__main__":
 
     strip_groups = []
     for x in range(0, 600, 60):
-        strip_groups.append(strips[x:x+60])
+        strip_groups.append(strips[x : x + 60])
 
     n = 0
     for strip_group in strip_groups:
@@ -104,4 +89,4 @@ if __name__ == "__main__":
         gdf.to_parquet(f"adriatic_strips/nucleus_test_{n}.parquet")
         n += 1
 
-    # gdf.to_parquet(f"{file}_centroids_.parquet")
+    # TODO merge all files >= nucleus_built-up_raw.parquet
