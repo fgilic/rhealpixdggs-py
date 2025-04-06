c37ff4652a25a750377e977189ab02c7e37e9f87
Frane Gilic <fgilic@gradst.hr>
2025-04-06 23:00:31 +0200

    Add updated modules for visualizations and output vector and raster images

---
+++ visualizations/visualization_3D.py
@@ -0,0 +1,145 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from shapely import LineString, Polygon
+from rhealpixdggs.ellipsoids import WGS84_ELLIPSOID, WGS84_ELLIPSOID_RADIANS
+from mayavi import mlab
+from rhealpixdggs.qpix_dggs import QPixDGGS
+from rhealpixdggs.dggs import RHEALPixDGGS
+import shapely
+import geopandas as gpd
+import mayavi
+
+mayavi.mlab.figure(bgcolor=(1,0,0), size=(1700, 1700))
+
+# EPSG:7030
+WGS84_A = WGS84_ELLIPSOID.a
+WGS84_F = WGS84_ELLIPSOID.f
+WGS84_B = WGS84_ELLIPSOID.b
+
+north_square = 0
+south_square = 0
+lon_0 = 0
+lat_0 = 0  # TODO
+ellipsoid = WGS84_ELLIPSOID_RADIANS
+ellipsoid.lon_0 = lon_0
+ellipsoid.lat_0 = lat_0
+
+# # plot sphere
+# la = np.linspace(-np.pi, np.pi, 100)
+# fi = np.linspace(-np.pi / 2, np.pi / 2, 100)
+# x = np.outer(np.cos(fi), np.cos(la))
+# y = np.outer(np.cos(fi), np.sin(la))
+# z = np.outer(np.sin(fi), np.ones(np.size(la)))
+# s = mlab.mesh(x, y, z, color = (0.5,0.5,0.5))
+
+# plot WGS84 ellipsoid
+a = WGS84_A
+b = WGS84_A
+e_2 = (a ** 2 - b ** 2) / a ** 2
+la = np.linspace(-np.pi, np.pi, 100)
+fi = np.linspace(-np.pi / 2, np.pi / 2, 100)
+x = np.outer(a / np.sqrt(1 - e_2 * (np.sin(fi)) ** 2) * np.cos(fi), np.cos(la))
+y = np.outer(a / np.sqrt(1 - e_2 * (np.sin(fi)) ** 2) * np.cos(fi), np.sin(la))
+z = np.outer(
+    a / np.sqrt(1 - e_2 * (np.sin(fi)) ** 2) * np.sin(fi), np.ones(np.size(la))
+)
+s = mlab.mesh(x, y, z, color=(1,1,1))  # color=(0.513, 0.596, 0.725)
+
+rdggs = QPixDGGS(ellipsoid=ellipsoid, N_side=3)
+cells = rdggs.grid(resolution=0)
+for cell in cells:
+    cell_boundary = cell.boundary(n=10, plane=False, interior=True)
+    points_3d = []
+    for point in cell_boundary:
+        lam, phi = point
+        n = a / math.sqrt(1 - e_2 * (math.sin(phi)) ** 2)
+        x = n * math.cos(phi) * math.cos(lam)
+        y = n * math.cos(phi) * math.sin(lam)
+        z = n * math.sin(phi)
+        points_3d.append((x,y,z))
+    polygon_3d = Polygon(points_3d)
+    x, y, z = list(zip(*list(polygon_3d.boundary.coords)))
+    mlab.plot3d(x, y, z, color=(0, 0.5882, 0.62745), tube_radius=130000)
+
+cells = rdggs.grid(resolution=1)
+for cell in cells:
+    cell_boundary = cell.boundary(n=10, plane=False, interior=True)
+    points_3d = []
+    for point in cell_boundary:
+        lam, phi = point
+        n = a / math.sqrt(1 - e_2 * (math.sin(phi)) ** 2)
+        x = n * math.cos(phi) * math.cos(lam)
+        y = n * math.cos(phi) * math.sin(lam)
+        z = n * math.sin(phi)
+        points_3d.append((x,y,z))
+    polygon_3d = Polygon(points_3d)
+    x, y, z = list(zip(*list(polygon_3d.boundary.coords)))
+    mlab.plot3d(x, y, z, color=(0, 0.5882, 0.62745), tube_radius=60000)
+
+
+cells = rdggs.grid(resolution=2)
+for cell in cells:
+    cell_boundary = cell.boundary(n=10, plane=False, interior=True)
+    points_3d = []
+    for point in cell_boundary:
+        lam, phi = point
+        n = a / math.sqrt(1 - e_2 * (math.sin(phi)) ** 2)
+        x = n * math.cos(phi) * math.cos(lam)
+        y = n * math.cos(phi) * math.sin(lam)
+        z = n * math.sin(phi)
+        points_3d.append((x,y,z))
+    polygon_3d = Polygon(points_3d)
+    x, y, z = list(zip(*list(polygon_3d.boundary.coords)))
+    mlab.plot3d(x, y, z, color=(0.808, 0.157, 0.447), tube_radius=35000)
+
+coastline_geometry = gpd.read_file("coastline_simplified.geojson").geometry
+coastline_gdf = gpd.GeoDataFrame({"geometry": coastline_geometry})
+
+for linestring in coastline_geometry:
+    points_3d = []
+    for point in linestring.coords:
+        lam, phi = point
+        lam = lam * math.pi / 180
+        phi = phi * math.pi / 180
+        n = a / math.sqrt(1 - e_2 * (math.sin(phi)) ** 2)
+        x = n * math.cos(phi) * math.cos(lam)
+        y = n * math.cos(phi) * math.sin(lam)
+        z = n * math.sin(phi)
+        points_3d.append((x,y,z))
+    if len(points_3d) < 4:
+        polygon_3d = LineString(points_3d)
+        x, y, z = list(zip(*list(polygon_3d.coords)))
+    else:
+        polygon_3d = Polygon(points_3d)
+        x, y, z = list(zip(*list(polygon_3d.boundary.coords)))
+    mlab.plot3d(x, y, z, color=(0, 0, 0), tube_radius=30000)
+
+grid_geometry = gpd.read_file("grid_10deg.geojson").geometry
+grid_gdf = gpd.GeoDataFrame({"geometry": coastline_geometry})
+
+for linestring in grid_geometry:
+    points_3d = []
+    for point in linestring.coords:
+        lam, phi = point
+        lam = lam * math.pi / 180
+        phi = phi * math.pi / 180
+        n = a / math.sqrt(1 - e_2 * (math.sin(phi)) ** 2)
+        x = n * math.cos(phi) * math.cos(lam)
+        y = n * math.cos(phi) * math.sin(lam)
+        z = n * math.sin(phi)
+        points_3d.append((x,y,z))
+    if len(points_3d) < 4:
+        polygon_3d = LineString(points_3d)
+        x, y, z = list(zip(*list(polygon_3d.coords)))
+    else:
+        polygon_3d = Polygon(points_3d)
+        x, y, z = list(zip(*list(polygon_3d.boundary.coords)))
+    mlab.plot3d(x, y, z, color=(0.6, 0.6, 0.6), tube_radius=15000)
+
+
+mlab.gcf().scene.parallel_projection = True
+mayavi.mlab.savefig("output.png")
+mlab.show()
--- visualizations/worldmap_qpix.py
+++ visualizations/worldmap_qpix.py
@@ -12,8 +12,9 @@ from rhealpixdggs.projection_wrapper import Projection
 from itertools import pairwise
 import numpy as np
 from rhealpixdggs.utils import auth_lat
-from rhealpixdggs.dggs import RHEALPixDGGS
+from rhealpixdggs.qpix_dggs import QPixDGGS
 from matplotlib import font_manager
+import math
 
 fig = plt.figure(figsize=(20, 10), frameon=False)
 ax = fig.add_subplot(1, 1, 1)
@@ -27,23 +28,46 @@ ellipsoid = WGS84_ELLIPSOID
 ellipsoid.lon_0 = lon_0
 ellipsoid.lat_0 = lat_0
 
-rhealpix_projection = Projection(
+rosca_plonka_projection = Projection(
     ellipsoid=ellipsoid,
-    proj="rhealpix",
+    proj="rosca_plonka",
     north_square=north_square,
     south_square=south_square,
 )
 phi_0_auth = np.arcsin(2.0 / 3) * 180 / np.pi  # phi_0 on sphere
 phi_0 = auth_lat(phi_0_auth, e=WGS84_ELLIPSOID.e, inverse=True)  # phi_0 on ellipsoid
 
-# coastline from cartopy (Naturel Earth)
+# coastline from cartopy (Natural Earth)
 # coastline_geometry = list(cartopy.feature.NaturalEarthFeature(category="physical", name="coastline", scale="110m").geometries())
 
 # coastline from cartopy exported and generalized/simplified in QGIS
 coastline_geometry = gpd.read_file("coastline_simplified.geojson").geometry
 coastline_gdf = gpd.GeoDataFrame({"geometry": coastline_geometry})
 
-# projecting coastline with rhealpix projection
+
+
+def region_from_point(p):
+    # determine resolution 0 cell from (lam, phi)
+    # expects ellipsoidal coordinates in degrees
+    lam, phi_ellips = p
+    lam = lam * math.pi / 180.0
+    phi_ellips = phi_ellips * math.pi / 180.0
+    phi = auth_lat(phi_ellips, e=WGS84_ELLIPSOID.e, inverse=False, radians=True)
+    lam_r = lam - math.pi / 4
+    x_r = math.cos(phi) * math.cos(lam_r)
+    y_r = math.cos(phi) * math.sin(lam_r)
+    z_r = math.sin(phi)
+
+    if x_r == z_r or -x_r == z_r or y_r == z_r or -y_r == z_r:
+        return "on_plane"
+    if z_r > x_r and z_r > -x_r and z_r > y_r and z_r > -y_r:
+        return "north_polar"
+    elif z_r <= x_r and z_r <= -x_r and z_r <= y_r and z_r <= -y_r:
+        return "south_polar"
+    else:
+        return "equatorial"
+
+# projecting coastline with rosca_plonka projection
 projected_lines = []
 for linestring in coastline_geometry:
     # LineString to lines between each vertex (so that lines crossing phi_0 can be omitted from visualization)
@@ -52,6 +76,8 @@ for linestring in coastline_geometry:
     for line in lines:
         point_1 = line[0]
         point_2 = line[1]
+        point_1_region = region_from_point(point_1)
+        point_2_region = region_from_point(point_2)
         if point_1[0] == 180.0:
             point_1 = (179.99999, point_1[1])
         if point_1[0] == -180.0:
@@ -60,52 +86,19 @@ for linestring in coastline_geometry:
             point_2 = (179.99999, point_2[1])
         if point_2[0] == -180.0:
             point_2 = (-179.99999, point_2[1])
-        if (
-            (point_1[1] < phi_0 < point_2[1])
-            or (point_1[1] > phi_0 > point_2[1])
-            or (point_1[1] < -phi_0 < point_2[1])
-            or (point_1[1] > -phi_0 > point_2[1])
-        ):
-            crossing_line = shapely.LineString([point_1, point_2])
-            if point_1[1] < phi_0 < point_2[1]:
-                phi_0_line = shapely.LineString(
-                    [(-180, phi_0 - 0.00001), (180, phi_0 - 0.00001)]
-                )
-                add_to_lat_next_point = 0.00001 * 2
-            if point_1[1] > phi_0 > point_2[1]:
-                phi_0_line = shapely.LineString(
-                    [(-180, phi_0 + 0.00001), (180, phi_0 + 0.00001)]
-                )
-                add_to_lat_next_point = -0.00001 * 2
-            if point_1[1] < -phi_0 < point_2[1]:
-                phi_0_line = shapely.LineString(
-                    [(-180, -phi_0 - 0.00001), (180, -phi_0 - 0.00001)]
-                )
-                add_to_lat_next_point = 0.00001 * 2
-            if point_1[1] > -phi_0 > point_2[1]:
-                phi_0_line = shapely.LineString(
-                    [(-180, -phi_0 + 0.00001), (180, -phi_0 + 0.00001)]
-                )
-                add_to_lat_next_point = -0.00001 * 2
-            intersection_point = crossing_line.intersection(phi_0_line)
-            point_1 = rhealpix_projection(point_1[0], point_1[1])
+        if (point_1_region != point_2_region):
+            point_1 = rosca_plonka_projection(point_1[0], point_1[1])
             point_list.append(point_1)
-            intersection_point_projected = rhealpix_projection(
-                intersection_point.x, intersection_point.y
-            )
-            point_list.append(intersection_point_projected)
-            projected_lines.append(shapely.LineString(point_list))
-            next_point_projected = rhealpix_projection(
-                intersection_point.x, intersection_point.y + add_to_lat_next_point
-            )
-            point_list = [next_point_projected]
+            if len(point_list) > 1:
+                projected_lines.append(shapely.LineString(point_list))
+            point_list = []
             continue
         # project points
-        point_1 = rhealpix_projection(point_1[0], point_1[1])
+        point_1 = rosca_plonka_projection(point_1[0], point_1[1])
         point_list.append(point_1)
 
     if len(point_list) > 1:
-        point_2 = rhealpix_projection(point_2[0], point_2[1])
+        point_2 = rosca_plonka_projection(point_2[0], point_2[1])
         point_list.append(point_2)
         projected_lines.append(shapely.LineString(point_list))
 
@@ -115,7 +108,7 @@ coastline_gdf.plot(ax=ax, edgecolor="black", linewidth=2, zorder=2)
 grid_gdf = gpd.read_file("grid_10deg.geojson")
 grid_geometry = grid_gdf.geometry
 
-# projecting grid with rhealpix projection
+# projecting grid with rosca_plonka projection
 projected_lines = []
 for linestring in grid_geometry:
     # LineString to lines between each vertex (so that lines crossing phi_0 can be omitted from visualization)
@@ -124,21 +117,16 @@ for linestring in grid_geometry:
     for line in lines:
         point_1 = line[0]
         point_2 = line[1]
-        if (
-            (point_1[1] < phi_0 < point_2[1])
-            or (point_1[1] > phi_0 > point_2[1])
-            or (point_1[1] < -phi_0 < point_2[1])
-            or (point_1[1] > -phi_0 > point_2[1])
-            or abs(point_1[1] == phi_0)
-            or abs(point_2[1] == phi_0)
-        ):
-            point_1 = rhealpix_projection(point_1[0], point_1[1])
+        point_1_region = region_from_point(point_1)
+        point_2_region = region_from_point(point_2)
+        if (point_1_region != point_2_region):
+            point_1 = rosca_plonka_projection(point_1[0], point_1[1])
             point_list.append(point_1)
             projected_lines.append(shapely.LineString(point_list))
             point_list = []
             continue
         # project points
-        point_1 = rhealpix_projection(point_1[0], point_1[1])
+        point_1 = rosca_plonka_projection(point_1[0], point_1[1])
         point_list.append(point_1)
 
     if len(point_list) > 1:
@@ -180,20 +168,14 @@ border_geometry = [
     ),
 ]
 
-# projecting border with rhealpix projection
-projected_lines = []
-for linestring in border_geometry:
-    # LineString to lines between each vertex
-    lines = list(zip(list(linestring.xy[0]), list(linestring.xy[1])))
-    point_list = []
-    for point in lines:
-        # project point
-        point = rhealpix_projection(point[0], point[1])
-        point_list.append(point)
-    projected_lines.append(shapely.LineString(point_list))
 
-border_gdf = gpd.GeoDataFrame({"geometry": projected_lines})
-# border_gdf.plot(ax=ax, edgecolor="black", linewidth=2.5, zorder=3)
+rdggs = QPixDGGS(ellipsoid=ellipsoid, N_side=3)
+
+border_points = [rdggs.ul_vertex["N"], (rdggs.ul_vertex["P"][0], rdggs.ul_vertex["N"][1]), rdggs.ul_vertex["P"], (rdggs.ul_vertex["R"][0] + (rdggs.ul_vertex["R"][0] - rdggs.ul_vertex["Q"][0]), rdggs.ul_vertex["P"][1]), (rdggs.ul_vertex["R"][0] + (rdggs.ul_vertex["R"][0] - rdggs.ul_vertex["Q"][0]), rdggs.ul_vertex["S"][1]), (rdggs.ul_vertex["P"][0], rdggs.ul_vertex["S"][1]), (rdggs.ul_vertex["P"][0], rdggs.ul_vertex["S"][1] - (rdggs.ul_vertex["O"][1] - rdggs.ul_vertex["S"][1])), (rdggs.ul_vertex["S"][0], rdggs.ul_vertex["S"][1] - (rdggs.ul_vertex["O"][1] - rdggs.ul_vertex["S"][1]))]
+border_geom = shapely.LinearRing(border_points)
+
+border_gdf = gpd.GeoDataFrame({"geometry": border_geom, "index":[1]})
+border_gdf.plot(ax=ax, edgecolor="black", linewidth=2.5, zorder=3)
 
 
 # Add font for figure
@@ -204,7 +186,7 @@ plt.rcParams["font.family"] = "sans-serif"
 plt.rcParams["font.sans-serif"] = prop.get_name()
 
 # plotting DGGS cells
-rdggs = RHEALPixDGGS(ellipsoid=ellipsoid, N_side=3)
+
 # resolution = 0
 # cells = rdggs.grid(resolution=0)
 # for cell in cells:
@@ -213,7 +195,7 @@ rdggs = RHEALPixDGGS(ellipsoid=ellipsoid, N_side=3)
 #     cell_gdf = gpd.GeoDataFrame([{"geometry": cell_geometry}])
 #     cell_gdf.plot(ax=ax, edgecolor="#0096a0", linewidth=2.5, zorder=4)
 #     # backgroundcolor=(1,1,1,0.5)
-#     plt.text(cell.centroid(plane=True)[0], cell.centroid(plane=True)[1], str(cell), family="sans-serif", ha="center",va="center", fontsize=40, color="#0096a0")
+#     plt.text(cell.nucleus(plane=True)[0], cell.nucleus(plane=True)[1], str(cell), family="sans-serif", ha="center",va="center", fontsize=40, color="#0096a0")
 
 # resolution = 1
 cells = rdggs.grid(resolution=1)
@@ -223,8 +205,8 @@ for cell in cells:
     cell_gdf = gpd.GeoDataFrame([{"geometry": cell_geometry}])
     cell_gdf.plot(ax=ax, edgecolor="#0096a0", linewidth=2.5, zorder=4)
     plt.text(
-        cell.centroid(plane=True)[0],
-        cell.centroid(plane=True)[1],
+        cell.nucleus(plane=True)[0],
+        cell.nucleus(plane=True)[1],
         str(cell),
         family="sans-serif",
         ha="center",
@@ -234,5 +216,5 @@ for cell in cells:
         backgroundcolor=(1, 1, 1, 0.5),
     )
 
-plt.savefig("rhealpix_world_level_1.svg")
+plt.savefig("qpix_world_level_1.svg")
 plt.show()
